5a59f42a3e628ab62b4c050edd9528554b1acc0a
Cam Zhou <wzhou@hmc.edu>
2015-11-13 20:51:03 -0800

    RapGenerator

---
+++ AutoRap/AutoRap/RapGenerator.swift
@@ -0,0 +1,17 @@
+//
+//  RapGenerator.swift
+//  AutoRap
+//
+//  Created by Cam on 11/13/15.
+//  Copyright © 2015 Cameric. All rights reserved.
+//
+
+import Foundation
+
+
+class RapGenerator {
+    func generateLyrics(keyword: String) -> String {
+        let lyrics = keyword
+        return lyrics
+    }
+}
--- AutoRap/AutoRap/ViewController.swift
+++ AutoRap/AutoRap/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var display_: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
